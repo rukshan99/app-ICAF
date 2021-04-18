640153265ec3be5a851487b416b45d8ef1aaf93b
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-04-18 15:36:51 +0530

    Basic navbar implemented

--- src/Shared/Navigation/NavLinks.js
+++ src/Shared/Navigation/NavLinks.js
@@ -5,49 +5,57 @@ import { NavLink } from 'react-router-dom';
 import './NavLinks.css';
 
 const NavLinks = props => {
-    const auth = "NOT NULL";// useContext(AuthContext);
+    const auth = "";// useContext(AuthContext);
 
     if(!auth) {
         return (
             <ul className="nav-links">
       <li>
         <NavLink to="/" exact>
-          Users
+          Admin
         </NavLink>
       </li>
         <li>
+          <NavLink to="/">Editor</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Reviewer</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">User</NavLink>
+        </li>
+        <li>
           <NavLink to="/auth">Sign in</NavLink>
         </li>
-      
     </ul>
         );
     }
-else {
-  return (
-    <ul className="nav-links">
-      <li>
-        <NavLink to="/" exact>
-          Users
-        </NavLink>
-      </li>
-      {auth.isSignedIn && (
-        <li>
-          <NavLink to={`/${auth.userId}/places`}>My places</NavLink>
-        </li>
-      )}
-      {auth.isSignedIn && (
-        <li>
-          <NavLink to="/places/new">New place</NavLink>
-        </li>
-      )}
-      {!auth.isSignedIn && (
-        <li>
-          <NavLink to="/auth">Sign in</NavLink>
-        </li>
-      )}
-    </ul>
-  );
-      }
+// else {
+//   return (
+//     <ul className="nav-links">
+//       <li>
+//         <NavLink to="/" exact>
+//           Users
+//         </NavLink>
+//       </li>
+//       {auth.isSignedIn && (
+//         <li>
+//           <NavLink to={`/${auth.userId}/places`}>My places</NavLink>
+//         </li>
+//       )}
+//       {auth.isSignedIn && (
+//         <li>
+//           <NavLink to="/places/new">New place</NavLink>
+//         </li>
+//       )}
+//       {!auth.isSignedIn && (
+//         <li>
+//           <NavLink to="/auth">Sign in</NavLink>
+//         </li>
+//       )}
+//     </ul>
+//   );
+//       }
 };
 
 export default NavLinks;
